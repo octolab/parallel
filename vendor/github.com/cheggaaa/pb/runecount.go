35d0ac419a305aaabc94cdfe70138d098dba4304
Kamil Samigullin <kamil@samigullin.info>
2019-03-01 22:25:49 +0300

    port cmd/semaphore as is

---
+++ vendor/github.com/cheggaaa/pb/runecount.go
@@ -0,0 +1,18 @@
+package pb
+
+import (
+	"regexp"
+
+	runewidth "github.com/mattn/go-runewidth"
+)
+
+// Finds the control character sequences (like colors)
+var ctrlFinder = regexp.MustCompile("\x1b\x5b[0-9]+\x6d")
+
+func escapeAwareRuneCountInString(s string) int {
+	n := runewidth.StringWidth(s)
+	for _, sm := range ctrlFinder.FindAllString(s, -1) {
+		n -= runewidth.StringWidth(sm)
+	}
+	return n
+}
